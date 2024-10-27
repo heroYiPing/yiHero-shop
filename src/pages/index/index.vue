09676869f5ffcf9ec50be5dbbfe6736a09fc246e
heroYiPing <1084811235@qq.com>
2024-10-27 14:32:24 +0800

    提交

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '../../services/home.js'
 import CustomNavbar from '../../pages/index/components/CustomNavbar.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+
+const bannerList = ref([])
+const getHomeBannerData = async () => {
+  let res = await getHomeBannerAPI()
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
