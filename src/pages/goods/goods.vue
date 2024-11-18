4476dbaf2f67a4875f70428353a5c8c02b21e5f1
heroYiPing <1084811235@qq.com>
2024-11-18 21:31:59 +0800

    商品详情页弹出层交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -3,6 +3,8 @@ import { onLoad } from '@dcloudio/uni-app'
 import { getGoodsByIdAPI } from '../../services/goods'
 import type { GoodsResult } from '../../types/goods'
 import { ref } from 'vue'
+import AddressPanel from './components/AddressPanel.vue'
+import ServicePanel from './components/ServicePanel.vue'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -31,6 +33,20 @@ const onTapImage = (url: string) => {
     current: url,
     urls: goods.value!.mainPictures,
   })
+}
+
+// uni-ui弹出层组件ref
+const popup = ref<{
+  open: (type?: UniHelper.UniPopupType) => void
+  close: () => void
+}>()
+
+// 弹出层条件渲染
+const popupName = ref<'service' | 'address'>()
+const openPopup = (name: typeof popupName.value) => {
+  // 修改弹出层名字
+  popupName.value = name
+  popup.value?.open()
 }
 
 // 页面加载
@@ -73,11 +89,11 @@ onLoad(() => {
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('address')">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('service')">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -146,6 +162,11 @@ onLoad(() => {
       <view class="addcart"> 加入购物车 </view>
       <view class="buynow"> 立即购买 </view>
     </view>
+
+    <uni-popup ref="popup" type="bottom" background-color="#fff">
+      <ServicePanel v-if="popupName == 'service'" @close="popup?.close()"></ServicePanel>
+      <AddressPanel v-else @close="popup?.close()"></AddressPanel>
+    </uni-popup>
   </view>
 </template>
 
