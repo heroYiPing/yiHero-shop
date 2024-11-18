d1d18b376bf54c96277edeb8ae78f1ddc49da80f
heroYiPing <1084811235@qq.com>
2024-11-18 23:44:47 +0800

    登录快捷登录

---
+++ src/services/login.ts
@@ -0,0 +1,29 @@
+import type { LoginResult } from '../types/member'
+import { http } from '../utils/http'
+
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+type simpleLoginParams = {
+  phoneNumber: string
+}
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
+export const postSimpleLoginWxMinAPI = (data: simpleLoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data,
+  })
+}
